ddbbe8fe38c863845b6601fcd03fa95b89e3c3fb
fab7891 <fabrice_nokam@yahoo.fr>
2021-09-09 15:43:02 +0200

    Read servive get result by command

--- src/main/java/com/example/remoteshellinux/service/CommandService.java
+++ src/main/java/com/example/remoteshellinux/service/CommandService.java
@@ -1,7 +1,12 @@
 package com.example.remoteshellinux.service;
 
+import com.jcraft.jsch.JSchException;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 import java.util.List;
 
+
 public interface CommandService {
-    public List<String> getresltByCommand(String commandName);
+    public List<String> getresltByCommand(String commandName) throws JSchException, IOException;
 }
--- src/main/java/com/example/remoteshellinux/service/CommandServiceImpl.java
+++ src/main/java/com/example/remoteshellinux/service/CommandServiceImpl.java
@@ -1,7 +1,17 @@
 package com.example.remoteshellinux.service;
 
+import com.example.remoteshellinux.Entities.ConnectParams;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -9,8 +19,23 @@ public class CommandServiceImpl implements  CommandService{
 
     private ConnectionService connectionService;
     @Override
-    public List<String> getresltByCommand(String commandName) {
-       // if(connectionService.connection())
-        return null;
+    public List<String> getresltByCommand(String commandName) throws JSchException, IOException {
+        ConnectParams connectParams= new ConnectParams();
+        Session session= connectionService.connection(connectParams);
+        Channel channel= session.openChannel("exec");
+        InputStream in=channel.getInputStream();
+        ((ChannelExec) channel).setCommand(commandName);
+        ((ChannelExec) channel).setErrStream(System.err);
+        channel.connect();
+        BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(in));
+        String line =bufferedReader.readLine();
+        List<String> result= new ArrayList<>();
+        while (line!=null){
+            result.add(line);
+            System.out.println(line);
+        }
+        channel.disconnect();
+        session.disconnect();
+        return result;
     }
 }
