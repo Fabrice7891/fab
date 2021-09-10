8b6d9b8bf85dd204002b928ccd3986c7fc05edea
Alex Cheuko <alexcheuko89@gmail.com>
2021-09-10 15:14:02 -0400

    remote shell command

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,7 +3,7 @@ var stompClient = null;
 
  function executeCommand() {
                 var command = document.getElementById('command').value;
-                stompClient.send("/app/execute", {},
+                  stompClient.send("/app/execute", {},
                   JSON.stringify({'command':command}));
             }
 
@@ -11,7 +11,6 @@ var stompClient = null;
                  var socket = new SockJS('/execute');
                  stompClient = Stomp.over(socket);
                  stompClient.connect({}, function(frame) {
-                     setConnected(true);
                      console.log('Connected: ' + frame);
                      stompClient.subscribe('/command/output', function(commandOutput) {
                          showCommandOutput(commandOutput.body);
@@ -21,9 +20,6 @@ var stompClient = null;
 
  function showCommandOutput(commandOutput) {
                  var response = document.getElementById('response');
-                 var p = document.createElement('p');
-                 p.style.wordWrap = 'break-word';
-                 p.appendChild(document.createTextNode(commandOutput));
-                 response.appendChild(p);
+                 response.appendChild(document.createTextNode(commandOutput));
              }
 
